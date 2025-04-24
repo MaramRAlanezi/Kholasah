67e7c5a9eb04e884020a7eed95f20e6d8c260b0a
MaramRAlanezi <malanezi24@twq.idserve.net>
2025-04-24 11:50:03 +0300

    Initial Commit

---
+++ Kholasah/KholasahApp.swift
@@ -0,0 +1,17 @@
+//
+//  KholasahApp.swift
+//  Kholasah
+//
+//  Created by Maram Rabeh  on 24/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct KholasahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KholasahTests/KholasahTests.swift
@@ -0,0 +1,17 @@
+//
+//  KholasahTests.swift
+//  KholasahTests
+//
+//  Created by Maram Rabeh  on 24/04/2025.
+//
+
+import Testing
+@testable import Kholasah
+
+struct KholasahTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
