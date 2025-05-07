da0491153424656cf17fef6e89d286ba5f0af952
Walghamdi97 <wejgh1997@gmail.com>
2025-05-07 11:31:03 +0300

    First Push (Not Ensurs)

---
+++ Kholasah/MeetingDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  MeetingDetailsView.swift
+//  Kholasah
+//
+//  Created by Wejdan Alghamdi on 08/11/1446 AH.
+//
+
+import SwiftUI
+
+struct MeetingDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeetingDetailsView()
+}
---
+++ Kholasah/PlayBackButtonControl.swift
@@ -0,0 +1,18 @@
+//
+//  PlayBackButtonControl.swift
+//  Kholasah
+//
+//  Created by Wejdan Alghamdi on 08/11/1446 AH.
+//
+
+import SwiftUI
+
+struct PlayBackButtonControl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlayBackButtonControl()
+}
