6962e34c39716c14cb4129f08266f5b92c07c7ac
Shaden14 <shadenabdullah14@gmail.com>
2025-05-11 11:02:00 +0300

    Subscription page

---
+++ Kholasah/View/PremiumPlanView.swift
@@ -0,0 +1,18 @@
+//
+//  PremiumPlanView.swift
+//  Kholasah
+//
+//  Created by Shaden Alhumood on 13/11/1446 AH.
+//
+
+import SwiftUI
+
+struct PremiumPlanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PremiumPlanView()
+}
