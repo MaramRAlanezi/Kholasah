7c9ffc144eb008e79f3e1b8eeb79c779bdabec20
Walghamdi97 <wejgh1997@gmail.com>
2025-05-11 10:21:02 +0300

    Audio Transcript Update

---
+++ Kholasah/TestAudioTranscript.swift
@@ -0,0 +1,18 @@
+//
+//  TestAudioTranscript.swift
+//  Kholasah
+//
+//  Created by Wejdan Alghamdi on 13/11/1446 AH.
+//
+
+import SwiftUI
+
+struct TestAudioTranscript: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestAudioTranscript()
+}
